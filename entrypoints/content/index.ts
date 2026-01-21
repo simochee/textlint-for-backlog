afa28adbcf71f25e84be96a23928480367258486
Ryoya Tamura <ryoya.tamura@nulab.com>
2026-01-21 18:42:37 +0900

    feat: setup textlint worker

--- entrypoints/content/index.ts
+++ entrypoints/content/index.ts
@@ -1,9 +1,16 @@
-import { defineContentScript } from 'wxt/utils/define-content-script';
+import { defineContentScript } from "wxt/utils/define-content-script";
 
 export default defineContentScript({
-  matches: ['https://*.backlog.com/*', "https://*.backlog.jp/*"],
-  allFrames: true,
-  async main() {
-    console.log('Content script for Backlog loaded');
-  }
-})
+	matches: ["https://*.backlog.com/*", "https://*.backlog.jp/*"],
+	allFrames: true,
+	async main() {
+		const res = await fetch(
+			"https://cdn.jsdelivr.net/gh/simochee/textlint-for-backlog/vendor/textlint-worker.js",
+		);
+		const script = await res.text();
+		const blob = new Blob([script], { type: "application/javascript" });
+		const worker = new Worker(URL.createObjectURL(blob));
+
+		worker.onmessage = console.log;
+	},
+});
